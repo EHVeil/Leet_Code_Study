8c4ffe16b0d136224d4c9c75ad88f4eb075c0122
Eli Veilleux <elibchv@gmail.com>
2022-10-17 13:45:11 -0700

    Tested theoretical solution

--- Longest_Substring_No_Repeats/longest_Substring.js
+++ Longest_Substring_No_Repeats/longest_Substring.js
@@ -1,18 +1,22 @@
 var lengthOfLongestSubstring = function(s) {
   //iterate through the string
+  var longest = 0;
   for (var i = 0; i < s.length; i++) {
-    var longest = 0;
     //create a list of encoutered characters in the current string
     var encountered =  {};
-    //while any encountered letters have not been encountered already iterate through the string from i index
     var j = i;
     var currLength = 0;
-    while(!encountered[s[i]]) {
+    //while any new letters have not been marked as encountered iterate through the string from i index
+    while(!encountered[s[j]] && j <= s.length - i) {
       //if a character has been encountered mark it
-      ecountered[s[i]] = true;
+      encountered[s[j]] = true;
       currLength = currLength + 1;
       j++;
     }
     //if a character is encountered twice, save the length of the string if its longer than any previous strings
+    if (currLength > longest) {
+      longest = currLength;
+    }
   }
+  return longest;
 };
