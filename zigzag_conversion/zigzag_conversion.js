65c5c2f59beff5974dbc744d79d4f8c978260ca1
Eli Veilleux <elibchv@gmail.com>
2022-11-03 16:09:03 -0700

    solution

--- zigzag_conversion/zigzag_conversion.js
+++ zigzag_conversion/zigzag_conversion.js
@@ -4,25 +4,33 @@
  * @return {string}
  */
  var convert = function(s, numRows) {
+  if (numRows === 1 || s.length <= 1) {
+      return s;
+  }
   //the number of rows determines how many characters will be in the 0 index of each row
   var rows = [];
   //iterate through the string and push r characters to the rows array
   var i = 0;
-  var r = 0;
+  var currRow = 0;
   var dir = true;
   while (i < s.length) {
-    rows[r] = rows[r] + s[i];
-    if (r === numRows - 1) {
+      if (!rows[currRow]){
+          rows[currRow] = s[i];
+      } else {
+         rows[currRow] = rows[currRow] + s[i];
+      }
+    if (currRow === numRows - 1 || rows.length === numRows && currRow === 0) {
       dir = !dir;
     }
     if(dir) {
-      r++;
+      currRow++;
     } else {
-      r--;
+      currRow--;
     }
     i++;
+    console.log(rows);
   }
-  return rows.join();
+  return rows.join('');
   //once at the limit of r begin adding characters to the orevious rows until you reach 0
   //do this until the string is exhausted
 
