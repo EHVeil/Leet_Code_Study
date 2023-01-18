c2fb1f3ee2934436c0750cd04b49d96dee86a6e0
Eli Veilleux <elibchv@gmail.com>
2023-01-18 14:08:26 -0800

    solution for find pivot index

--- Find_Pivot_index/Find_Pivot_index.js
+++ Find_Pivot_index/Find_Pivot_index.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-
+  //create two vars that represent the sum to the left and right of the current index
+  var leftSum =  0;
+  var rightSum = 0;
+  for (var i = 1; i < nums.length; i++) {
+      rightSum += nums[i];
+  }
+  //iterate through the array of numbers
+  for (var i = 0; i < nums.length; i++) {
+      if (leftSum === rightSum) {
+          return i;
+      }
+      leftSum += nums[i];
+      rightSum -= nums[i + 1];
+  }
+  return -1;
+  //compare the 2 values, if equal return the current index
+  //on the next iteration add the value of the previous index to the left sum, and subtract the value of the current index from the right sum;
+  //if after iterating through the entire array no match is found retun -1
 };
+
+console.log(pivotIndex([1,7,3,6,5,6])); //3
