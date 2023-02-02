d9503e284488f5708e5119cf5b51e9b553f78ca8
Eli Veilleux <elibchv@gmail.com>
2023-02-02 14:13:38 -0800

    initialize edit

---
+++ Verification_Alien_Alphabet/verificationAlienAlphabet.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function(words, order) {
+  //iterate through the set of words
+  //compare 2 words at a time using the order supplied as an argument
+  //
+ };
